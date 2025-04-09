69068dd1646a55b97f6a09aa76e9f9c0eed3c9ae
Yosri Bedoui <yosri.bedoui@outlook.com>
2025-04-09 21:06:49 +0100

    chore: console log

--- main.py
+++ main.py
@@ -11,15 +11,7 @@ def main():
     print("Some HR niggers say they wanna see the contributions chart glow green")
     print("Others say they wanna see some 'smart AI' shit on my github profile (I'm a javascript developer applying for web dev positions -_-)")
     print("¯\_(ツ)_/¯")
-    print("Commit 1")
-    print("Commit 2")
-    print("Commit 3")
-    print("Commit 4")
-    print("Commit 5")
-    print("Commit 6")
-    print("Commit 7")
-    print("Commit 8")
-    print("Commit 9")
+   
     example_function()
 
 if __name__ == "__main__":
